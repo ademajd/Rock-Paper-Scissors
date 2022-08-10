e3c25e52d3afcb7a2565bdf595363510639d1545
ademajd <48736756+ademajd@users.noreply.github.com>
2022-08-11 01:36:33 +0200

    Main logic is done but have to work on what to show in DOM

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const rockB = document.querySelector("#rock");
+const paperB = document.querySelector("#paper");
+const scissorsB = document.querySelector("#scissors");
+const gameWinner = document.querySelector(".game-winner")
+const gameMoves = document.querySelectorAll(".move")
+
+const winner = document.createElement("div");
+winner.classList.add("winner");
+gameWinner.appendChild(winner);
+
+
+
 function getComputerChoice() {
     let rand = Math.floor(Math.random()* 10);
 
@@ -10,10 +22,7 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-function getPlayerInput() {
-    const playerInput = prompt("Let´s play rock paper scissors. Which one do you chose?").toLowerCase();
-    return playerInput;
-}
+
 
 
 function getComputerInput() {
@@ -21,9 +30,10 @@ function getComputerInput() {
     return computerInput;
 }
 
-function playGame() {
-    getPlayer = getPlayerInput();
-    getComputer = getComputerInput();
+function playGame(playerInput) {
+    getPlayer = playerInput;
+    // getComputer = getComputerInput();
+    getComputer = "rock";
     const rock = "rock";
     const paper = "paper";
     const scissors = "scissors"
@@ -36,9 +46,9 @@ function playGame() {
 
     if (getPlayer == rock) {
         if(getComputer == paper) {
-            return "computer wins"
+            return "Computer Wins"
         } else if (getComputer == scissors) {
-            return "You Wins";
+            return "You Win";
         }
     }
 
@@ -55,18 +65,48 @@ function playGame() {
             return "You Win";
         }
         else if (getComputer == rock) {
-            return "Computer wins";
+            return "Computer Wins";
         }
         
     }
 }
 
 
-function game() {
-    for (let index = 0; index < 5; index++) {
-        console.log(playGame());
-    }
+function playFiveRound() {
+    let pcWins = 0;
+    let playerWins = 0;
+
+    gameMoves.forEach(element => {
+        element.addEventListener("click",() => {
+            if (winner.childNodes.length != 0) {
+                gameWinner.removeChild(winner);
+            }
+            let roundResult = playGame(element.id);
+            console.log("-----------------------");
+            if (roundResult == "You Win") {
+                playerWins++;
+            } else if (roundResult == "Computer Wins") {
+                pcWins++;
+            }
+            
+            if (pcWins == 5) {
+                winner.textContent = "pcWins";
+                pcWins = 0;
+                return;
+            } else if (playerWins == 5) {
+                winner.textContent = "playerWins";
+                playerWins = 0;
+                return;
+            }
+            
+        })
+    
+        
+    });
+    
 }
 
+playFiveRound();
 
-game();
+
+
